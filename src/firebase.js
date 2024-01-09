58952067fbef203a538cb5b0ab9076c3bdcfe20b
Tushar <tusharchemate07@gmail.com>
2024-01-09 21:23:12 +0530

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAOjZF2vJG8B5rDPXsqJRhBrIdkWVCx4ww',
+  authDomain: 'clone-9d11f.firebaseapp.com',
+  projectId: 'clone-9d11f',
+  storageBucket: 'clone-9d11f.appspot.com',
+  messagingSenderId: '413953751953',
+  appId: '1:413953751953:web:b9fe54dc0e9d2662a57830',
+  measurementId: 'G-9HXHYBYYPN',
+};
