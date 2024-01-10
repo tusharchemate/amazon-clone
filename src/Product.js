00c81b3085ec6113356dbe5eb562090472c4a65d
Tushar <tusharchemate07@gmail.com>
2024-01-10 22:39:19 +0530

    added rating in product

--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = ({ title, image, price, rating }) => {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" />
+      <img src={image} />
       <button>Add to Basket</button>
     </div>
   );
