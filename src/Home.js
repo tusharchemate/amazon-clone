279c9dfbaee433216a5150675a170c9ec1970bd2
Tushar <tusharchemate07@gmail.com>
2024-01-10 22:10:10 +0530

    added product info

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 const Home = () => {
   return (
@@ -10,6 +11,14 @@ const Home = () => {
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt="image"
         />
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+
+        <div className="home__row"></div>
+
+        <div className="home__row"></div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './Product.css';
 
 const Product = () => {
-  return <div className="product">Product</div>;
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
