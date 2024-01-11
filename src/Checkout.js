addcc787ecd1401019dca9e1c3c2e665d2ef2700
Tushar <tusharchemate07@gmail.com>
2024-01-11 22:04:13 +0530

    added checkout layout in app

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Checkout.css';
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+        />
+
+        <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
+        </div>
+      </div>
+      <div>
+        <h2 className="checkout__right">The subtotal will go here</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
