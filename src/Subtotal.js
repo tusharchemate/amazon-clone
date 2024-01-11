b92f1e22a10d1094772fb9c737359210b26c923b
Tushar <tusharchemate07@gmail.com>
2024-01-11 22:30:16 +0530

    fixed subtotal css issue

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,8 +4,8 @@ import './Subtotal.css';
 const Subtotal = () => {
   return (
     <div className="subtotal">
-      <p>
-        Subtotal (0 items) : <strong>1</strong>
+      <p className="subtotal__price">
+        Subtotal (0 items) : <strong> 1</strong>
       </p>
       <small className="subtotal__gift">
         <input type="checkbox" />
