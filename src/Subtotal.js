046a975197de4429dd4a4d43d29d7759ac52f901
Tushar <tusharchemate07@gmail.com>
2024-01-11 22:27:34 +0530

    added subtotal in app

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Subtotal.css';
 
 const Subtotal = () => {
   return (
-    <div>Subtotal</div>
-  )
-}
+    <div className="subtotal">
+      <p>
+        Subtotal (0 items) : <strong>1</strong>
+      </p>
+      <small className="subtotal__gift">
+        <input type="checkbox" />
+        This offer contains a gift
+      </small>
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
 
-export default Subtotal
+export default Subtotal;
