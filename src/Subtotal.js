4512be522d91fdaf80e6740fc0364e2d6e3f13fa
Tushar <tusharchemate07@gmail.com>
2024-01-13 02:29:50 +0530

    addded subtotal in subtotal component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Subtotal.css';
+import { useStateValue } from './StateProvider';
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
+  let totalPrice = basket.reduce((total, item) => total + item.price, 0);
   return (
     <div className="subtotal">
       <p className="subtotal__price">
-        Subtotal (0 items) : <strong> 1</strong>
+        Subtotal ( {basket.length} items) : <strong> {totalPrice}</strong>
       </p>
       <small className="subtotal__gift">
         <input type="checkbox" />
