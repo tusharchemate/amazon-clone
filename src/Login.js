3cc90a454f98519206bdb8d24780c82b86c07566
Tushar <tusharchemate07@gmail.com>
2024-01-14 15:51:24 +0530

    added state in login component

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="login">
       <Link to={'/'}>
@@ -17,14 +20,22 @@ const Login = () => {
 
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="login__SignIn">Sign In</button>
         </form>
         <p>
-          By Signin you agree to amazon condition & Sale. Please seee our
-          privacy notice.
+          By Signin you agree to amazon condition & Sale. Please see our privacy
+          notice.
         </p>
         <button className="login__register">Create your Amazon Account</button>
       </div>
