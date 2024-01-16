8722e10945783f2d14e2730bbf61c4a81e26935f
Tushar <tusharchemate07@gmail.com>
2024-01-16 23:26:03 +0530

    added  axios

--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
 import CheckoutProduct from './CheckoutProduct';
 import { Link } from 'react-router-dom';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const Payment = () => {
   const [{ basket }, dispatch] = useStateValue();
+  let totalPrice = basket.reduce((total, item) => total + item.price, 0);
+
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [processing, setProcessing] = useState(false);
+  const [succeeded, setSucceeded] = useState(false);
+
+  const stripe = useStripe();
+  const element = useElements();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.
+  };
+
+  const handleChange = (event) => {
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : '');
+  };
+
   return (
     <div className="payment">
       <div className="payment__container">
@@ -46,7 +68,16 @@ const Payment = () => {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details"></div>
+          <div className="payment__details">
+            <form>
+              <CardElement onChange={handleChange} />
+              Subtotal ( {basket.length} items) : <strong> {totalPrice}</strong>
+              <button disabled={processing || disabled || succeeded}>
+                <span>{processing ? <p>Processing</p> : 'Buy Now'}</span>
+              </button>
+              {error && <div>{error}</div>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
